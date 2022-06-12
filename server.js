f5a938abafbd71b545c31ebc3cda218995bc3ed2
suhila1010 <sohilaahmed678@gmail.com>
2022-06-12 22:37:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,5 +7,14 @@ app.use(express.static('app'));
 app.use(express.static('build/contracts'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + './app/index')
+    res.sendFile(`${__dirname}./app/index.html`);
+})
+
+app.get("*", (req, res) => {
+    res.status(404);
+    res.send('Ooops... this url does not exist.')
+});
+
+app.listen(PORT,() => {
+    console.log(`App is running on ${PORT}`);
 })
